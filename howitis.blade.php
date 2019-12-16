9b2722e893582c62f74c1e5a60164716e3adee1d
Iskman <51114487+Iskman@users.noreply.github.com>
2019-12-17 05:35:29 +0600

    Add files via upload

---
+++ howitis.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+			@include('header')
+			<center>
+				<img src='http://pgu.iskakovstudio.kz/task.jpg'>
+				<p class='h4'>Была поставлена такая задача...</p><br>
+				<img src='http://pgu.iskakovstudio.kz/1.png' class='mt-3'>
+				<p class='h4'>Локальный сервер для тестирования</p><br>
+				<img src='http://pgu.iskakovstudio.kz/2.png'>
+				<p class='h4'>Создание локального домена</p><br>
+				<img src='http://pgu.iskakovstudio.kz/3.png'>
+				<p class='h4'>Установка Composer'а 1</p><br>
+				<img src='http://pgu.iskakovstudio.kz/4.png'>
+				<p class='h4'>Установка Composer'а 2</p><br>
+				<img src='http://pgu.iskakovstudio.kz/5.png'>
+				<p class='h4'>Установка Composer'а 3</p><br>
+				<img src='http://pgu.iskakovstudio.kz/6.png'>
+				<p class='h4'>Установка Laravel через Composer</p><br>
+				<img src='http://pgu.iskakovstudio.kz/7.png'>
+				<p class='h4'>Установка Laravel через Composer</p><br>
+				<img src='http://pgu.iskakovstudio.kz/8.png'>
+				<p class='h4'>Установка Laravel через Composer</p><br>
+				<img src='http://pgu.iskakovstudio.kz/9.png'>
+				<p class='h4'>Установка Laravel через Composer завершен</p><br>
+				<img src='http://pgu.iskakovstudio.kz/10.png'>
+				<p class='h4'>Проверка!</p><br>
+				<img src='http://pgu.iskakovstudio.kz/11.png'>
+				<p class='h4'>За работу в PHP, LARAVEL, MySQL, JavaScript, CSS и т.п.</p><br>
+				<img src='http://pgu.iskakovstudio.kz/Mansur.jpg'>
+				<p class='h4'>Программирование, тестирование и снова программирование...</p><br>
+			</center>
+		</div>
+    </body>
+</html>
