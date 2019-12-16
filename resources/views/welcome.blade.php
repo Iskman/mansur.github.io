087854a0c0f9c89e1dfabc0cacabe8ed8a35fe11
Iskman <51114487+Iskman@users.noreply.github.com>
2019-12-17 04:42:21 +0600

    Add files via upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 			@include('header')
 
-			<h3 class='text-muted'>ПОПУЛЯРНОЕ (статьи с наиболшим количеством просмотров)</h3>
+			<h3 class="text-muted">ПОПУЛЯРНОЕ (статьи с наиболшим количеством просмотров)</h3>
 			<ul class="list-unstyled">
 				@foreach ($popular as $prs)
-					<li>[{{ $loop->index }}]&nbsp;<a href="ajax/{{$prs->id}}" class='ajax'>{{ $prs->title }}</a></li>
+					<li><font class="text-muted">[{{ $loop->index+1 }}]</font>&nbsp;<a href="ajax/{{$prs->id}}" class='ajax'>{{ $prs->title }}</a></li>
 				@endforeach
 			</ul>
 			<h3 class='text-muted'>ВСЕ НОВОСТИ</h3>
 			<ul class="list-unstyled">
 				@foreach ($parsing as $prs)
-					<li>[{{ $prs->id }}]&nbsp;<a href="post/{{$prs->id}}">{{ $prs->title }}</a><p>{{ $prs->dsc }}</p></li>
+					<li><a href="post/{{$prs->id}}">{{ $prs->title }}</a><p>{{ $prs->dsc }}</p></li>
 				@endforeach
 			</ul>
 			{{$parsing->links()}}
